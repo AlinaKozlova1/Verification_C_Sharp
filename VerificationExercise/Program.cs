2940a928de6856ffac67c562d411f23aff2275da
Alina <alinakozlova908@gmail.com>
2022-08-17 15:11:25 +0300

    add manual input array values

--- VerificationExercise/Program.cs
+++ VerificationExercise/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter your array size: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array =  new string[n];
+
+
+void FillArray (string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine("Enter " + (i+1) + " array valye: ");
+        arr[i] = Console.ReadLine();
+    }
+    
+}
+
+FillArray(array);
+
+Console.WriteLine("Initial array: [" + string.Join(", ", array) + "]");
+
