9bfae1dddcf04b1426617a7150764c87d4b177e1
Alina <alinakozlova908@gmail.com>
2022-08-17 17:25:25 +0300

    did the main part of exercise

--- VerificationExercise/Program.cs
+++ VerificationExercise/Program.cs
@@ -2,7 +2,6 @@
 int n = Convert.ToInt32(Console.ReadLine());
 string[] array =  new string[n];
 
-
 void FillArray (string[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
@@ -14,6 +13,28 @@ void FillArray (string[] arr)
 }
 
 FillArray(array);
-
 Console.WriteLine("Initial array: [" + string.Join(", ", array) + "]");
+
+string[] RefillArray(string[] arr2)
+{
+    int k = 0;
+    for (int j = 0; j < arr2.Length; j++)
+    {
+        if (arr2[j].Length <= 3) k++;
+    }
+    string[] removeArr = new string[k];
+    int l = 0;
+        for (int i = 0; i < arr2.Length; i++)
+        {
+            if (arr2[i].Length <= 3)
+            {
+                removeArr[l] = arr2[i];
+                l++;
+            }
+        }
+    return removeArr;
+}
+
+Console.WriteLine("Output array: [" + string.Join(", ", RefillArray(array)) + "]");
+
 
